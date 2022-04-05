3337e24c12d584f2a9bf014514c0f8e59110d7cd
Jose Barros <jose.barros.s@ug.uchile.cl>
2022-04-05 02:04:04 -0400

    Changed bucket

--- disease_codification/gcp.py
+++ disease_codification/gcp.py
@@ -7,29 +7,29 @@ from dotenv import load_dotenv
 load_dotenv()
 
 storage_client = storage.Client(project=os.getenv("PROJECT_ID"))
-bucket = storage_client.bucket(os.getenv("BUCKET"))
+BUCKET = storage_client.bucket(os.getenv("BUCKET"))
 
 
 def upload_blob(python_obj: object, filename: str):
     """Uploads a python object to the bucket."""
     pickle_out = pickle.dumps(python_obj)
-    blob = bucket.blob(filename)
+    blob = BUCKET.blob(filename)
     blob.upload_from_string(pickle_out)
 
 
 def upload_blob_file(filename_in: object, filename_out: str):
     """Uploads a python object to the bucket."""
-    blob = bucket.blob(filename_out)
+    blob = BUCKET.blob(filename_out)
     blob.upload_from_filename(filename_in)
 
 
 def download_blob(filename: str):
     """Uploads a python object to the bucket."""
-    blob = bucket.blob(filename)
+    blob = BUCKET.blob(filename)
     pickle_in = blob.download_as_string()
     return pickle.loads(pickle_in)
 
 
 def download_blob_file(filename_in: str, filename_out: str):
-    blob = bucket.blob(filename_in)
+    blob = BUCKET.blob(filename_in)
     blob.download_to_filename(filename_out)
