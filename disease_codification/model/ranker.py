d05c916905eadbc6f2dcdb3e1f7f28d2f853617d
Jose Barros <jose.barros.s@ug.uchile.cl>
2022-04-11 15:18:04 -0400

    Made ranker able to extend to other embeddings beside tfidf

--- disease_codification/model/ranker.py
+++ disease_codification/model/ranker.py
@@ -21,15 +21,24 @@ from disease_codification.model.matcher import Matcher
 
 class Ranker:
     def __init__(
-        self, indexers_path: Path, models_path: Path, indexer: str, cluster_classifier={}, cluster_label_binarizer={}
+        self,
+        indexers_path: Path,
+        models_path: Path,
+        indexer: str,
+        cluster_classifier={},
+        cluster_label_binarizer={},
+        cluster_tfidf={},
+        embedding_type="tfidf",
     ):
         self.indexers_path = indexers_path
         self.models_path = models_path
         self.indexer = indexer
         self.cluster_classifier = cluster_classifier
         self.cluster_label_binarizer = cluster_label_binarizer
+        self.cluster_tfidf = cluster_tfidf
         self.mappings = load_pickle(self.indexers_path / self.indexer / "mappings.pickle")
         self.clusters = set(self.mappings.values())
+        self.embedding_type = embedding_type
         Ranker.create_directories(models_path, indexer)
 
     @classmethod
@@ -38,7 +47,9 @@ class Ranker:
         create_dir_if_dont_exist(models_path / indexer / "ranker")
 
     @classmethod
-    def load(cls, indexers_path: Path, models_path: Path, indexer: str, load_from_gcp: bool = False):
+    def load(
+        cls, indexers_path: Path, models_path: Path, indexer: str, load_from_gcp: bool = False, embedding_type="tfidf"
+    ):
         cls.create_directories(models_path, indexer)
         clusters = set(load_pickle(indexers_path / indexer / "mappings.pickle").values())
         cluster_classifier = {}
@@ -46,12 +57,24 @@ class Ranker:
         for cluster in clusters:
             label_binarizer_path = models_path / indexer / "ranker" / f"label_binarizer_{cluster}.pickle"
             classifier_path = models_path / indexer / "ranker" / f"classifier_{cluster}.pickle"
+            tfidf_path = models_path / indexer / "ranker" / f"tfidf_{cluster}.pickle"
             if load_from_gcp:
                 download_blob_file(f"{indexer}/ranker/label_binarizer_{cluster}.pickle", label_binarizer_path)
                 download_blob_file(f"{indexer}/ranker/classifier_{cluster}.pickle", classifier_path)
+                if embedding_type == "tfidf":
+                    download_blob_file(f"{indexer}/ranker/tfidf_{cluster}.pickle", tfidf_path)
             cluster_classifier[cluster] = load_pickle(classifier_path)
             cluster_label_binarizer[cluster] = load_pickle(label_binarizer_path)
-        return cls(indexers_path, models_path, indexer, cluster_classifier, cluster_label_binarizer)
+            cluster_tfidf = load_pickle(tfidf_path) if embedding_type == "tfidf" else {}
+        return cls(
+            indexers_path,
+            models_path,
+            indexer,
+            cluster_classifier,
+            cluster_label_binarizer,
+            cluster_tfidf,
+            embedding_type,
+        )
 
     def _read_sentences(self, cluster: str, split_types: List[str]) -> List[Sentence]:
         filename = f"ranker_{cluster}"
@@ -70,32 +93,40 @@ class Ranker:
             sentences += list(getattr(multi_corpus, split_type))
         return sentences
 
-    def _set_multi_label_binarizer(self, cluster, sentences):
+    def _set_multi_label_binarizer(self, cluster):
         mlb = MultiLabelBinarizer()
         labels_cluster = {f"<{label}>" for label, cluster_label in self.mappings.items() if cluster_label == cluster}
         mlb.fit([list(labels_cluster) + ["<incorrect-matcher>"]])
         self.cluster_label_binarizer[cluster] = mlb
+
+    def _set_tfidf(self, cluster):
+        tfidf = TfidfVectorizer()
+        sentences = [s.to_original_text() for s in self._read_sentences(cluster, ["train", "dev"])]
+        tfidf.fit(sentences)
+        self.cluster_tfidf[cluster] = tfidf
 
     def _get_labels_matrix(self, cluster, sentences):
         mlb = self.cluster_label_binarizer[cluster]
         labels_matrix = mlb.transform([[label.value for label in s.get_labels("gold")] for s in sentences])
         return labels_matrix
 
-    def _get_embeddings(self, sentences: List[Sentence]):
-        return [s.to_original_text() for s in sentences]
+    def _get_embeddings(self, cluster: str, sentences: List[Sentence]):
+        if self.embedding_type == "tfidf":
+            return self.cluster_tfidf[cluster].transform([s.to_original_text() for s in sentences])
 
     def train(self, upload_to_gcp: bool = False, split_types_train=["train", "dev"]):
         len_clusters = len(self.clusters)
         for i, cluster in enumerate(self.clusters):
             print(f"Training for cluster {cluster} - {i}/{len_clusters}")
+            self._set_multi_label_binarizer(cluster)
+            if self.embedding_type == "tfidf":
+                self._set_tfidf(cluster)
             sentences = self._read_sentences(cluster, split_types_train)
-            embeddings = self._get_embeddings(sentences)
-            self._set_multi_label_binarizer(cluster, sentences)
+            embeddings = self._get_embeddings(cluster, sentences)
             labels = self._get_labels_matrix(cluster, sentences)
             if not labels.any() or not sentences:
                 raise Exception
-            pipe = Pipeline([("tfidf", TfidfVectorizer()), ("xgboost", XGBClassifier(eval_metric="logloss"))])
-            clf = OneVsRestClassifier(pipe).fit(embeddings, labels)
+            clf = OneVsRestClassifier(XGBClassifier(eval_metric="logloss")).fit(embeddings, labels)
             self.cluster_classifier[cluster] = clf
         print("Training Complete")
         self.save()
@@ -106,7 +137,7 @@ class Ranker:
         print("Predicting ranker")
         for cluster in self.clusters:
             print(cluster)
-            embeddings = self._get_embeddings(sentences)
+            embeddings = self._get_embeddings(cluster, sentences)
             predictions = self.cluster_classifier[cluster].predict_proba(embeddings)
             classes = self.cluster_label_binarizer[cluster].classes_
             for sentence, prediction in zip(sentences, predictions):
@@ -119,7 +150,7 @@ class Ranker:
         for split_type in split_types:
             for cluster in self.clusters:
                 sentences = self._read_sentences(cluster, [split_type])
-                embeddings = self._get_embeddings(sentences)
+                embeddings = self._get_embeddings(cluster, sentences)
                 predictions = self.cluster_classifier[cluster].predict_proba(embeddings)
                 labels_matrix = self._get_labels_matrix(cluster, sentences)
                 avg_precs = []
@@ -138,6 +169,8 @@ class Ranker:
         for cluster in self.clusters:
             save_as_pickle(self.cluster_label_binarizer[cluster], base_path / f"label_binarizer_{cluster}.pickle")
             save_as_pickle(self.cluster_classifier[cluster], base_path / f"classifier_{cluster}.pickle")
+            if self.embedding_type == "tfidf":
+                save_as_pickle(self.cluster_tfidf[cluster], base_path / f"tfidf_{cluster}.pickle")
 
     def upload_to_gcp(self):
         base_path = self.models_path / self.indexer / "ranker"
@@ -149,3 +182,5 @@ class Ranker:
             upload_blob_file(
                 base_path / f"classifier_{cluster}.pickle", f"{self.indexer}/ranker/classifier_{cluster}.pickle"
             )
+            if self.embedding_type == "tfidf":
+                upload_blob_file(base_path / f"tfidf_{cluster}.pickle", f"{self.indexer}/ranker/tfidf_{cluster}.pickle")
