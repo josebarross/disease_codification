e4cbda10854e1a1c7d570cb19183ecd57cf69e0d
Jose Barros <jose.barros.s@ug.uchile.cl>
2022-04-20 13:35:17 -0400

    Download livingner corpus

--- disease_codification/corpora_downloader.py
+++ disease_codification/corpora_downloader.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from venv import create
 import zipfile
 import shutil
 import wget
@@ -14,15 +15,29 @@ def create_directories(data_path: Path):
 
 
 def download_codiesp_corpus(corpuses_path: Path):
-    if not Path(corpuses_path / "codiesp.zip").is_file():
-        url = "https://zenodo.org/record/3837305/files/codiesp.zip?download=1"
-        wget.download(url, f"{corpuses_path}/codiesp.zip")
-        with zipfile.ZipFile(f"{corpuses_path}/codiesp.zip", "r") as zf:
-            zf.extractall(corpuses_path)
-        shutil.move(corpuses_path / "final_dataset_v4_to_publish", corpuses_path / "codiesp")
-    if not Path(corpuses_path / "codiesp_codes.zip").is_file():
-        url = "https://zenodo.org/record/3632523/files/codiesp_codes.zip?download=1"
-        wget.download(url, f"{corpuses_path}/codiesp_codes.zip")
-        with zipfile.ZipFile(f"{corpuses_path}/codiesp_codes.zip", "r") as zf:
-            codes_path = create_dir_if_dont_exist(corpuses_path / "codiesp_codes")
-            zf.extractall(codes_path)
+    print("Downloading codiesp corpus")
+    url = "https://zenodo.org/record/3837305/files/codiesp.zip?download=1"
+    download_zipped_corpus(corpuses_path, "codiesp", url, old_name="final_dataset_v4_to_publish")
+    print("Downloading codiesp labels")
+    url = "https://zenodo.org/record/3632523/files/codiesp_codes.zip?download=1"
+    download_zipped_corpus(corpuses_path, "codiesp_codes", url, create_containing_folder=True)
+
+
+def download_livingner_corpus(corpuses_path: Path):
+    print("Downloading LIVINGNER corpus")
+    url = "https://zenodo.org/record/6421410/files/training_valid.zip?download=1"
+    download_zipped_corpus(corpuses_path, "livingner", url, create_containing_folder=True)
+
+
+def download_zipped_corpus(
+    corpuses_path: Path, corpus_name: str, url: str, old_name: str = None, create_containing_folder: bool = False
+):
+    if not Path(corpuses_path / f"{corpus_name}.zip").is_file():
+        wget.download(url, f"{corpuses_path}/{corpus_name}.zip")
+        with zipfile.ZipFile(f"{corpuses_path}/{corpus_name}.zip", "r") as zf:
+            path = corpuses_path
+            if create_containing_folder:
+                path = create_dir_if_dont_exist(corpuses_path / corpus_name)
+            zf.extractall(path)
+    if old_name:
+        shutil.move(corpuses_path / old_name, corpuses_path / corpus_name)
